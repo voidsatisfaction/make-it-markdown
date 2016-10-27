13c32b7523ceafddebc6d5d79cea4e1d43712b81
voidSatisfaction <lourie@naver.com>
2016-10-27 23:26:34 +0900

    parse html

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const htmlparser = require('htmlparser2');
 // get html from url
 const url = 'http://www.shayashi.jp/courses/2016/sui5kouki/20161026.html';
 
-function getHtml(url) {
+function getHtml(url, callback) {
+  var body = '';
   http.get(url, (res) => {
-    var body = '';
     res.setEncoding('utf8');
 
     res.on('data', (chunk) => {
       body += chunk;
     });
     res.on('end', (res) => {
-      console.log(body);
-      res = JSON.parse(body);
-      console.log(res);
+      // console.log(body);
+      // res = JSON.parse(body);
+      // console.log(res);
+      callback(body);
     });
   }).on('error', (err) => {
     console.log(`Something is error : ${err.message}`);
@@ -23,12 +24,26 @@ function getHtml(url) {
 }
 
 // make url to object
-const rawHtml = getHtml(url);
-const handler = new htmlparser.DomHandler((err, data) => {
-  console.log(data);
-});
-const parser = new htmlparser.Parser(handler);
-parser.write(rawHtml);
-parser.end();
+function parseHtml(rawHtml) {
+  var parser = new htmlparser.Parser({
+    onopentag: function(name, attribs){
+        if(name === "script" && attribs.type === "text/javascript"){
+            console.log("JS! Hooray!");
+        }
+    },
+    ontext: function(text){
+        console.log("-->", text);
+    },
+    onclosetag: function(tagname){
+        if(tagname === "script"){
+            console.log("That's it?!");
+        }
+    }
+  }, {decodeEntities: true});
+  parser.write(rawHtml);
+  parser.end();
+}
+
+getHtml(url, parseHtml);
 
 // change it to mdfile
