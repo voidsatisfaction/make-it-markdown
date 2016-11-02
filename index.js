85fb382bc778762a52f23c20d4270adfb40752e3
voidSatisfaction <lourie@naver.com>
2016-11-02 20:59:06 +0900

    add to-markdown library

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http');
 const fs = require('fs');
-const htmlparser = require('htmlparser2');
+const toMarkdown = require('to-markdown');
 
 // get html from url
-const url = 'http://www.shayashi.jp/courses/2016/sui5kouki/20161026.html';
+const url = 'http://www.shayashi.jp/courses/2016/sui5kouki/20161102.html';
 
 function getHtml(url, callback) {
+  function fileSave(md) {
+    fs.writeFile('test2.md', md, (err) => {
+      console.log(`Making md file error : ${err}`);
+    });
+  }
   var body = '';
   http.get(url, (res) => {
     res.setEncoding('utf8');
@@ -17,78 +22,18 @@ function getHtml(url, callback) {
       // console.log(body);
       // res = JSON.parse(body);
       // console.log(res);
-      callback(body, (data) => {
-        fs.writeFile('test.md', data, (err) => {
-          console.log(`Making md file error : ${err}`)
-        });
-      });
+      callback(body, fileSave);
     });
   }).on('error', (err) => {
     console.log(`html get request error : ${err.message}`);
   });
 }
 // make url to object
-function parseHtml(rawHtml, callback) {
-  function changeToMD(temp) {
-    const ignoreTag = {
-      div: 1, span: 1, br: 1,
-    };
-    const inDirectTag = {
-      ul: 1, ol: 1,
-    };
-    const directTag = {
-      h1: '#', h2: '##', h3: '###', h4: '####', h5: '#####',
-      hr: '---', li: '-', p: '\n'
-    };
-
-    const { tagName, attribs, text } = temp;
-    console.log(tagName)
-    // General case
-    if (ignoreTag[tagName]) {
-      return '';
-    } else if (inDirectTag[tagName]) {
-      temp.parent = tagName;
-      return '';
-    } else if (directTag[tagName] && text) {
-      return `${directTag[tagName]} ${text}${'\n'}`;
-    }
-    // Special case
-    switch (tagName) {
-      case 'a':
-        return `[${text}](${attribs.href})${'\n'}`;
-      default:
-        return '';
-    }
-  }
-  var data = '';
-  var temp = {};
-  var isBody = false;
-  const parser = new htmlparser.Parser({
-    onopentag(tagName, attribs) {
-      isBody = (tagName === 'body') ? true : isBody;
-      if (isBody) {
-        temp.tagName = tagName;
-        temp.attribs = attribs;
-      }
-    },
-    ontext(text) {
-      if (isBody) {
-        temp.text = text;
-      }
-    },
-    onclosetag(tagName) {
-      if (isBody) {
-        data += changeToMD(temp);
-        temp = {};
-      }
-    },
-  }, { decodeEntities: true });
-  parser.write(rawHtml);
-  parser.end();
-  console.log(data);
-  callback(data);
+function htmlToMarkdown(data, callback) {
+  const md = toMarkdown(data);
+  callback(md);
 }
 
-getHtml(url, parseHtml);
+getHtml(url, htmlToMarkdown);
 
 // change it to mdfile
