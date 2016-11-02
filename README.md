69b13fad6eb3b24e2561026dc3c6166864a9559a
voidSatisfaction <lourie@naver.com>
2016-11-03 00:06:23 +0900

    add argv packages

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Input html address then Output automated markdown file
 - General
   - [ ] 코드 정리
   - [ ] 터미널 상으로 url받기
+  - . / .. => url상의 주소로 변환
+  - node markdown "string" / node markdown -p "address1" "address2" "address3"
   - [ ] md파일 형식 : yy-mm-dd-url
 
 - API Network
   - [ ] Https 대응 
+
+- To-markdown 원리 탐구
--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const http = require('http');
 const fs = require('fs');
 const toMarkdown = require('to-markdown');
+const argv = require('argv');
 
 // get html from url
+const urls = argv.run().targets;
+
 const url = 'http://www.shayashi.jp/courses/2016/sui5kouki/20161102.html';
 
+function iterateOver(urls, iterator, callback) {
+  var doneCount = 0;
+  function report() {
+    doneCount += 1;
+
+    if (doneCount === urls.length) {
+      callback();
+    }
+  }
+
+  for(var i = 0; i < urls.length; i++) {
+    iterator(urls[i], report);
+  }
+}
+
 function getHtml(url, callback) {
-  function fileSave(md) {
-    fs.writeFile('test2.md', md, (err) => {
+  function fileSave(md, callback) {
+    fs.writeFile(`${ new Date() }test.md`, md, (err) => {
       console.log(`Making md file error : ${err}`);
     });
+    callback();
   }
   var body = '';
   http.get(url, (res) => {
@@ -34,6 +53,9 @@ function htmlToMarkdown(data, callback) {
   callback(md);
 }
 
-getHtml(url, htmlToMarkdown);
+iterateOver(urls, (url, report) => {
+  getHtml(url, htmlToMarkdown);
+
+}, () => console.log('all done'));
 
 // change it to mdfile
